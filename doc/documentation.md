4efba1edd096dcc2ed64cfff8cd8a0b031ed8af3
ghhggg123456 <olshu10@mail.ru>
2026-06-09 12:02:24 +0300

    add functions of turns and doc for it

--- doc/documentation.md
+++ doc/documentation.md
@@ -1,20 +1,40 @@
-#### mollusk.forward(\[distance])
+# Mollusk documentation
+
+Table of contents: 
+- [Standard functions](#standard-functions)
+  - [Functions of basic movement](#functions-of-basic-movement)
+    - [mollusk.forward()](#molluskforward)
+    - [mollusk.left()](#molluskleft)
+    - [mollusk.right()](#molluskright)
+    - [mollusk.back()](#molluskback)
+    - [mollusk.up()](#molluskup)
+    - [mollusk.down()](#molluskdown)
+  - [Functions of directional change](#functions-of-directional-change)
+    - [mollusk.turnLeft()](#molluskturnleft)
+    - [mollusk.turnRight()](#molluskturnright)
+
+
+## Standard functions
+
+### Functions of basic movement
+
+#### mollusk.forward()
 ```
 mollusk.forward([distance])
 ```
-Moves the turtle forward by `distance` blocks. Can handle negative numbers (it will work like opposite function).
+Moves the turtle forward by `distance` blocks. Can handle negative numbers (it will work like moving opposite direction).
 ##### Parameters
-1. distance=1? : number The number of blocks to move.
+1. distance=1 : number The number of blocks to move.
 ##### Returns
 1. boolean Whether the movement completed successfully.
 2. string | nil The reason for stopping or nil if successful.
 3. number The number of blocks actually moved (equals `distance` if successful).
 
-#### mollusk.left(\[distance])
+#### mollusk.left()
 ```
 mollusk.left([distance])
 ```
-Moves the turtle left by `distance` blocks, keeping the starting direction. (turtle turns left, moves `distance` blocks forward, and turns right afterwards). Can handle negative numbers (it will work like opposite function).
+Moves the turtle left by `distance` blocks, keeping the starting direction. (turtle turns left, moves `distance` blocks forward, and turns right afterwards). Can handle negative numbers (it will work like moving opposite direction, turtle turns left, moves `distance` blocks back, and turns right afterwards).
 ##### Parameters
 1. distance=1 : number The number of blocks to move.
 ##### Returns
@@ -22,11 +42,11 @@ Moves the turtle left by `distance` blocks, keeping the starting direction. (tur
 2. string | nil The reason for stopping or nil if successful.
 3. number The number of blocks actually moved (equals `distance` if successful).
 
-#### mollusk.right(\[distance])
+#### mollusk.right()
 ```
 mollusk.right([distance])
 ```
-Moves the turtle right by `distance` blocks, keeping the starting direction. (turtle turns right, moves `distance` blocks forward, and turns left afterwards). Can handle negative numbers (it will work like opposite function).
+Moves the turtle right by `distance` blocks, keeping the starting direction. (turtle turns right, moves `distance` blocks forward, and turns left afterwards). Can handle negative numbers (it will work like moving opposite direction, turtle turns right, moves `distance` blocks back, and turns left afterwards).
 ##### Parameters
 1. distance=1 : number The number of blocks to move.
 ##### Returns
@@ -34,23 +54,11 @@ Moves the turtle right by `distance` blocks, keeping the starting direction. (tu
 2. string | nil The reason for stopping or nil if successful.
 3. number The number of blocks actually moved (equals `distance` if successful).
 
-#### mollusk.back(\[distance])
+#### mollusk.back()
 ```
 mollusk.back([distance])
 ```
-Moves the turtle back by `distance` blocks. Can handle negative numbers (it will work like opposite function).
-##### Parameters
-1. distance=1 : number The number of blocks to move, keeping the starting direction.
-##### Returns
-1. boolean Whether the movement completed successfully.
-2. string | nil The reason for stopping or nil if successful.
-3. number The number of blocks actually moved (equals `distance` if successful).
-
-#### mollusk.up(\[distance])
-```
-mollusk.up([distance])
-````
-Moves the turtle up by `distance` blocks. Can handle negative numbers (it will work like opposite function).
+Moves the turtle back by `distance` blocks. Can handle negative numbers (it will work like moving opposite direction).
 ##### Parameters
 1. distance=1 : number The number of blocks to move.
 ##### Returns
@@ -58,14 +66,52 @@ Moves the turtle up by `distance` blocks. Can handle negative numbers (it will w
 2. string | nil The reason for stopping or nil if successful.
 3. number The number of blocks actually moved (equals `distance` if successful).
 
-#### mollusk.down(\[distance])
+#### mollusk.up()
+```
+mollusk.up([distance])
+```
+Moves the turtle up by `distance` blocks. Can handle negative numbers (it will work like moving opposite direction).
+##### Parameters
+1. distance=1 : number The number of blocks to move.
+##### Returns
+1. boolean Whether the movement completed successfully.
+2. string | nil The reason for stopping or nil if successful.
+3. number The number of blocks actually moved (equals `distance` if successful).
+
+#### mollusk.down()
 ```
 mollusk.down([distance])
 ```
-Moves the turtle down by `distance` blocks. Can handle negative numbers (it will work like opposite function).
+Moves the turtle down by `distance` blocks. Can handle negative numbers (it will work like moving opposite direction).
 ##### Parameters
 1. distance=1 : number The number of blocks to move.
 ##### Returns
 1. boolean Whether the movement completed successfully.
 2. string | nil The reason for stopping or nil if successful.
 3. number The number of blocks actually moved (equals `distance` if successful).
+
+### Functions of directional change
+
+#### mollusk.turnLeft()
+```
+mollusk.turnLeft([count])
+```
+Rotate the turtle left 90 degrees `count` times
+##### Parameters
+1. count=1 : number The number of turns. Can handle negative numbers (it will work like opposite function).
+##### Returns
+1. boolean Whether the rotation completed successfully.
+2. string | nil The reason for stopping or nil if successful.
+3. number The number of times actually turned (by 90 degrees) (equals `count` if successful).
+
+#### mollusk.turnRight()
+```
+mollusk.turnRight([count])
+```
+Rotate the turtle right 90 degrees `count` times
+##### Parameters
+1. count=1 : number The number of turns. Can handle negative numbers (it will work like opposite function).
+##### Returns
+1. boolean Whether the rotation completed successfully.
+2. string | nil The reason for stopping or nil if successful.
+3. number The number of times actually turned (by 90 degrees) (equals `count` if successful).
--- mollusk.lua
+++ mollusk.lua
@@ -1,110 +1,154 @@
 function forward(distance)
     distance = distance or 1
+    local check, reason = true, nil
+    local distanceAbs = math.abs(distance)
     if distance>0 then 
-        for block=0, distance-1 do
-            local check, reason = turtle.forward()
-            if check == false then return false, reason, block end
+        for block=0, distanceAbs-1 do
+            check, reason = turtle.forward()
+            if check == false then return check, reason, block end
         end
     elseif distance<0 then
-        local distanceAbs = math.abs(distance)
         for block=0, distanceAbs-1 do
-            local check, reason = turtle.back()
-            if check == false then return false, reason, block end
+            check, reason = turtle.back()
+            if check == false then return check, reason, block end
         end
     end
-    return true, nil, distance
+    return check, reason, distance
 end
 
 function left(distance)
     distance = distance or 1
+    local check, reason = true, nil
+    local distanceAbs = math.abs(distance)
+    if distance == 0 then return true, nil, 0 end
+    check, reason = turtle.turnLeft()
+    if check == false then return false, reason, 0 end
     if distance>0 then 
-        turtle.turnLeft()
-        for block=0, distance-1 do
-            local check, reason = turtle.forward()
-            if check == false then return false, reason, block end
-        end
-        turtle.turnRight()
-    elseif distance<0 then
-        local distanceAbs = math.abs(distance)
-        turtle.turnRight()
         for block=0, distanceAbs-1 do
-            local check, reason = turtle.forward()
-            if check == false then return false, reason, block end
+            check, reason = turtle.forward()
+            if check == false then return check, reason, block end
         end
-        turtle.turnLeft()
+    elseif distance<0 then
+        for block=0, distanceAbs-1 do
+            check, reason = turtle.back()
+            if check == false then return check, reason, block end
+        end
     end
-    return true, nil, distance
+    check, reason = turtle.turnRight()
+    return check, reason, distance
 end
 
 function right(distance)
     distance = distance or 1
+    local check, reason = true, nil
+    local distanceAbs = math.abs(distance)
+    if distance == 0 then return true, nil, 0 end
+    check, reason = turtle.turnRight()
+    if check == false then return false, reason, 0 end
     if distance>0 then 
-        turtle.turnRight()
-        for block=0, distance-1 do
-            local check, reason = turtle.forward()
-            if check == false then return false, reason, block end
-        end
-        turtle.turnLeft()
-    elseif distance<0 then
-        local distanceAbs = math.abs(distance)
-        turtle.turnLeft()
         for block=0, distanceAbs-1 do
-            local check, reason = turtle.forward()
-            if check == false then return false, reason, block end
+            check, reason = turtle.forward()
+            if check == false then return check, reason, block end
         end
-        turtle.turnRight()
+    elseif distance<0 then
+        for block=0, distanceAbs-1 do
+            check, reason = turtle.back()
+            if check == false then return check, reason, block end
+        end
     end
-    return true, nil, distance
+    check, reason = turtle.turnLeft()
+    return check, reason, distance
 end
 
 function back(distance)
     distance = distance or 1
+    local check, reason = true, nil
+    local distanceAbs = math.abs(distance)
     if distance>0 then 
-        for block=0, distance-1 do
-            local check, reason = turtle.back()
-            if check == false then return false, reason, block end
+        for block=0, distanceAbs-1 do
+            check, reason = turtle.back()
+            if check == false then return check, reason, block end
         end
     elseif distance<0 then
-        local distanceAbs = math.abs(distance)
         for block=0, distanceAbs-1 do
-            local check, reason = turtle.forward()
-            if check == false then return false, reason, block end
+            check, reason = turtle.forward()
+            if check == false then return check, reason, block end
         end
     end
-    return true, nil, distance
+    return check, reason, distance
 end
+
 
 function up(distance)
     distance = distance or 1
+    local check, reason = true, nil
+    local distanceAbs = math.abs(distance)
     if distance>0 then 
-        for block=0, distance-1 do
-            local check, reason = turtle.up()
-            if check == false then return false, reason, block end
+        for block=0, distanceAbs-1 do
+            check, reason = turtle.up()
+            if check == false then return check, reason, block end
         end
     elseif distance<0 then
-        local distanceAbs = math.abs(distance)
         for block=0, distanceAbs-1 do
-            local check, reason = turtle.down()
-            if check == false then return false, reason, block end
+            check, reason = turtle.down()
+            if check == false then return check, reason, block end
         end
     end
-    return true, nil, distance
+    return check, reason, distance
 end
 
 function down(distance)
     distance = distance or 1
+    local check, reason = true, nil
+    local distanceAbs = math.abs(distance)
     if distance>0 then 
-        for block=0, distance-1 do
-            local check, reason = turtle.down()
-            if check == false then return false, reason, block end
+        for block=0, distanceAbs-1 do
+            check, reason = turtle.down()
+            if check == false then return check, reason, block end
         end
     elseif distance<0 then
-        local distanceAbs = math.abs(distance)
         for block=0, distanceAbs-1 do
-            local check, reason = turtle.up()
-            if check == false then return false, reason, block end
+            check, reason = turtle.up()
+            if check == false then return check, reason, block end
         end
     end
-    return true, nil, distance
+    return check, reason, distance
 end
+
+function turnLeft(count)
+    count = count or 1
+    local check, reason = true, nil
+    local countAbs = math.abs(count)
+    if count>0 then
+        for step=0, countAbs-1 do
+            check, reason = turtle.turnLeft()
+            if check == false then return check, reason, step end
+        end
+    elseif count<0 then
+        for step=0, countAbs-1 do
+            check, reason = turtle.turnRight()
+            if check == false then return check, reason, step*-1 end
+        end
+    end
+    return check, reason, count
+end
+
+function turnRight(count)
+    count = count or 1
+    local check, reason = true, nil
+    local countAbs = math.abs(count)
+    if count>0 then
+        for step=0, countAbs-1 do
+            check, reason = turtle.turnRight()
+            if check == false then return check, reason, step end
+        end
+    elseif count<0 then
+        for step=0, countAbs-1 do
+            check, reason = turtle.turnLeft()
+            if check == false then return check, reason, step*-1 end
+        end
+    end
+    return check, reason, count
+end
+
 
